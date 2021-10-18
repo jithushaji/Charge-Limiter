dd2a2af79bb63826911fed108396263d1b5fcfcb
jithu_shaji <jithushaji23@gmail.com>
2021-10-18 10:01:36 +0530

    fix foe runnig on startup

--- Charge_limiter.py
+++ Charge_limiter.py
@@ -3,16 +3,24 @@ from tkinter import *
 
 def Chrg_lmt():
     lmt= str(enter.get())
+    #power_cfg = open("/sys/class/power_supply/BAT0/charge_control_end_threshold", "r")
+    #print("Current Charge Threshold:")
+    #print(power_cfg.read())
     power_cfg = open("/sys/class/power_supply/BAT0/charge_control_end_threshold", "w")
     print("Updating Threshold.....")
     power_cfg.write(lmt);
     print("Threshold Set To: ",lmt)
 
 
+
 def add(event):
     a=int(enter.get())
     b=a+1
-    enter.set(b)
+    if(b<=100):
+        enter.set(b)
+    else:
+        b=100
+        enter.set(b)
     return
 
 
@@ -24,28 +32,40 @@ def sub(event):
 
 #******* GUI code***********
 
+
+
 root=Tk()
 root.geometry('240x90')
 root.title('Charge Limiter')
-
 power_cfg = open("/sys/class/power_supply/BAT0/charge_control_end_threshold", "r")
 pwr_val=int(power_cfg.read())
-enter=IntVar(value = pwr_val)
 
-label=Label(root,text="Set Limit",bg='skyblue',fg='red').grid(row=2,column=1)
 
-entry_1=Entry(root,textvariable=enter).grid(row=3,column=1)
+if(pwr_val==100):
+    enter=IntVar(value = "80")
+    Chrg_lmt()
+    exit(0)
 
-button1=Button(root,text='+')
-button1.grid(row=3,column=2)
-button1.bind('<Button-1>',add)
+else:
 
-button2=Button(root,text='-')
-button2.grid(row=3,column=0)
-button2.bind('<Button-1>',sub)
+    power_cfg = open("/sys/class/power_supply/BAT0/charge_control_end_threshold", "r")
+    pwr_val=int(power_cfg.read())
+    enter=IntVar(value = pwr_val)
 
-button3=Button(root,text='Apply', command=Chrg_lmt)
-button3.grid(row=4,column=1)
-button3.bind()
+    label=Label(root,text="Set Limit",bg='skyblue',fg='red').grid(row=2,column=1)
 
-root.mainloop()
+    entry_1=Entry(root,textvariable=enter).grid(row=3,column=1)
+
+    button1=Button(root,text='+')
+    button1.grid(row=3,column=2)
+    button1.bind('<Button-1>',add)
+
+    button2=Button(root,text='-')
+    button2.grid(row=3,column=0)
+    button2.bind('<Button-1>',sub)
+
+    button3=Button(root,text='Apply', command=Chrg_lmt)
+    button3.grid(row=4,column=1)
+    button3.bind()
+
+    root.mainloop()
